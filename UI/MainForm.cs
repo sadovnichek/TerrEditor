a9fb837f42a10a97a371aa9f87ef99e121b02f2a
sadovnichek <kapkaevictor@gmail.com>
2022-04-26 22:28:35 +0500

    добавлен UI

--- UI/MainForm.cs
+++ UI/MainForm.cs
@@ -5,5 +5,30 @@ public partial class MainForm : Form
     public MainForm()
     {
         InitializeComponent();
+        WindowState = FormWindowState.Maximized;
+        var layout = GetFlowLayoutPanel();
+        layout.Controls.Add(GetStartButton());
+        layout.Controls.Add(GetStartButton());
+        layout.Controls.Add(GetStartButton());
+        layout.Controls.Add(GetStartButton());
+        Controls.Add(layout);
+    }
+
+    private FlowLayoutPanel GetFlowLayoutPanel()
+    {
+        return new FlowLayoutPanel()
+        {
+            Dock = DockStyle.Top,
+            AutoSize = true,
+            FlowDirection = FlowDirection.LeftToRight
+        };
+    }
+
+    private Button GetStartButton()
+    {
+        return new Button()
+        {
+            Size = new Size(100, 100)
+        };
     }
 }
